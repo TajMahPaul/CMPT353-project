380a94d5c0c79cf9c465903651efa9a758437547
JBHoang <jasonbuihoang@gmail.com>
2020-08-07 18:18:48 -0700

    filtered df to show only data where peaks are

--- get_features.py
+++ get_features.py
@@ -7,13 +7,27 @@ from scipy.signal import chirp, find_peaks, peak_widths
 RAW_DATA_DIRECTORY= "./raw_data"
 
 def get_peaks(x, y):
-    peaks, _ = find_peaks(y, height=.1)
+    peaks, _ = find_peaks(y, height=.5)
     plt.plot(x,y)
     plt.plot(peaks, y[peaks], "x")
+    return peaks
     
 
-def get_features(x, y, name):
-    peaks = get_peaks(x,y)
+def get_features(df, name):
+    peaks = get_peaks(df['time'],df['a_mag'])
+    
+    # Filters the df to show only data where the peaks are
+    df_peaks = df.loc[peaks]
+    print(df_peaks)
+
+    # should be the value from the next row, then drops last value because after it's shifted it will be null
+    df_shifted = df[name].shift(periods=-1) 
+    df_shifted = df_shifted[df_shifted.notnull()]
+
+    # TODO: Filters peaks that are close together in a_mag
+    
+
+    # TODO: find the distance between 2 points in a_mag
 
 
 def process_data(path):
@@ -24,10 +38,12 @@ def process_data(path):
     
     columns = ['ax', 'ay', 'az', 'a_mag']
     for column in columns:
-        get_features(df_right_leg['time'],df_right_leg['a_mag'], 'a_mag')
-        get_features(df_right_leg['time'],df_right_leg['az'], 'az')
-        get_features(df_right_leg['time'],df_right_leg['ay'], 'ay')
-        get_features(df_right_leg['time'],df_right_leg['ax'], 'ax')
+        get_features(df_right_leg, 'a_mag')
+        
+        # get_features(df_right_leg['time'],df_right_leg['a_mag'], 'a_mag')
+        # get_features(df_right_leg['time'],df_right_leg['az'], 'az')
+        # get_features(df_right_leg['time'],df_right_leg['ay'], 'ay')
+        # get_features(df_right_leg['time'],df_right_leg['ax'], 'ax')
         plt.show()
         break
 
