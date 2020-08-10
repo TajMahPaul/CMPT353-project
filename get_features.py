03dc3005e23e8f3bec866fa50c4a4079d7a341b2
JBHoang <jasonbuihoang@gmail.com>
2020-08-09 19:02:25 -0700

    made graphs and seperated peaks

--- get_features.py
+++ get_features.py
@@ -2,32 +2,66 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import chirp, find_peaks, peak_widths
+from scipy.signal import chirp, find_peaks, peak_widths, argrelextrema
+from datetime import datetime
+import statistics
 
 RAW_DATA_DIRECTORY= "./raw_data"
+
+mean_mag_right_leg = []
 
 def get_peaks(x, y):
     peaks, _ = find_peaks(y, height=.5)
     plt.plot(x,y)
     plt.plot(peaks, y[peaks], "x")
     return peaks
-    
+
+def get_width(y, peaks):
+    width = peak_widths(y, peaks, rel_height=0.9)
+    plt.hlines(*width[1:], color="C2")
+    return width
+
+def get_min(df):
+    df['min'] = df.iloc[argrelextrema(df.a_mag.values, np.less_equal, order=5)[0]]['a_mag']
+    plt.scatter(df.index,df['min'], c='r')
+
+    # returns indices of min values
+    return df[df['min'].notnull()].index.tolist()
 
 def get_features(df, name):
     peaks = get_peaks(df['time'],df['a_mag'])
+    width = get_width(df['a_mag'], peaks)
+    min_indices = get_min(df)
+    plt.show()
+   
+    n = len(min_indices)
+    single_peak_indices = []
+    for i in range(n):
+        start_number = min_indices[i]
+        if(i != n-1):
+            end_number = min_indices[i+1]
+
+        while (start_number <= end_number):
+            single_peak_indices.append(start_number)
+            start_number = start_number + 1
+        
+        single_peak = df.loc[single_peak_indices]
+        
+        # filters out small peaks
+        if (single_peak['a_mag'].mean() > 0.1):
+            x = list(range(len(single_peak)))
+            plt.plot(x, single_peak['a_mag'])
+
+            mean = single_peak['a_mag'].mean()
+            mean_mag_right_leg.append(mean)
+
+        single_peak_indices.clear()
+
+        
+   
+    total_mean_mag_right_leg = statistics.mean(mean_mag_right_leg)
+    print(total_mean_mag_right_leg)
     
-    # Filters the df to show only data where the peaks are
-    df_peaks = df.loc[peaks]
-    print(df_peaks)
-
-    # should be the value from the next row, then drops last value because after it's shifted it will be null
-    df_shifted = df[name].shift(periods=-1) 
-    df_shifted = df_shifted[df_shifted.notnull()]
-
-    # TODO: Filters peaks that are close together in a_mag
-    
-
-    # TODO: find the distance between 2 points in a_mag
 
 
 def process_data(path):
