74347041af7bf44ed478e6376852ba6e531ee182
Paul Dhanda <pauldhanda@live.com>
2020-08-11 17:43:59 -0700

    resolved conflicts

--- get_features.py
+++ get_features.py
@@ -6,6 +6,9 @@ from scipy.signal import chirp, find_peaks, peak_widths, argrelextrema
 from datetime import datetime
 import statistics
 from scipy.integrate import simps
+from scipy.interpolate import interp1d
+from scipy.spatial.distance import euclidean
+from fastdtw import fastdtw
 
 RAW_DATA_DIRECTORY= "./raw_data"
 
@@ -20,6 +23,37 @@ def get_distance_per_step(df):
 def get_velocity_per_step(df):
     return simps(df['a_mag'])
 
+def filter_peaks(segPeaks, name):
+
+    # returns the peaks with lowest DWS distance to every other peak (ie. the peak more like all the other peaks)
+    average_distances = []
+    
+    for i in range(len(segPeaks)):
+        distances = []
+
+        # normalize time from 0 - 1 for each peak and linear interpolation to have equal points and distance between points for good comparison
+        normal_time_i = (segPeaks[i]['time']-segPeaks[i]['time'].min())/(segPeaks[i]['time'].max()-segPeaks[i]['time'].min())
+        new_f_i = interp1d(normal_time_i, segPeaks[i][name])
+        new_x_i = np.linspace(0, 1, num=50, endpoint=True)
+        new_y_i = new_f_i(new_x_i)
+
+        for j in range(len(segPeaks)):
+            if(i != j):
+
+                # normalize time from 0 - 1 for each peak and linear interpolation to have equal points and distance between points for good comparison
+                normal_time_j = (segPeaks[j]['time']-segPeaks[j]['time'].min())/(segPeaks[j]['time'].max()-segPeaks[j]['time'].min())
+                new_f_j = interp1d(normal_time_j, segPeaks[j][name])
+                new_x_j = np.linspace(0, 1, num=50, endpoint=True)
+                new_y_j = new_f_j(new_x_j)
+
+                distance, path = fastdtw(new_y_i, new_y_j, dist=euclidean)
+                distances.append(distance)
+
+        avg = sum(distances) / len(distances)
+        average_distances.append((i, avg))
+
+    print(average_distances)
+    return segPeaks[min(average_distances, key = lambda t: t[1])[0]]
 
 def get_peaks(x, y):
     peaks, _ = find_peaks(y, height=.5)
@@ -43,11 +77,15 @@ def get_features(df, name, path):
     peaks = get_peaks(df['time'],df[name])
     width = get_width(df[name], peaks)
     min_indices = get_min(df)
-    # plt.show()
+    
+    segmented_peaks = []
 
     list_of_means = []
-    features = []
-   
+    features = {
+        "labels": [],
+        "values": []
+    }
+    
     n = len(min_indices)
     single_peak_indices = []
     for i in range(n):
@@ -63,26 +101,39 @@ def get_features(df, name, path):
         
         # filters out small peaks
         if (single_peak[name].mean() > 0.1):
+
+            # get elapse time 
             single_peak['time'] = get_elapse_time(single_peak['time'])
-            plt.plot(single_peak['time'], single_peak[name])
+
+            segmented_peaks.append(single_peak)
 
             mean = single_peak[name].mean()
             list_of_means.append(mean)
 
         single_peak_indices.clear()
 
-    # plt.show()
+    best_peak = filter_peaks(segmented_peaks, name)
+    plt.plot(best_peak['time'], best_peak['a_mag'])
+    plt.show()
 
     # convert list of means into pandas DataFrame
     df_list_mean = pd.DataFrame(data=list_of_means)
 
     # append to features the min, max, mean, median, variance, std
-    features.append(df_list_mean.min())
-    features.append(df_list_mean.max())
-    features.append(df_list_mean.mean())
-    features.append(df_list_mean.median())
-    features.append(df_list_mean.var())
-    features.append(df_list_mean.std())
+    features.values.append(df_list_mean.min())
+    features.values.append(df_list_mean.max())
+    features.values.append(df_list_mean.mean())
+    features.values.append(df_list_mean.median())
+    features.values.append(df_list_mean.var())
+    features.values.append(df_list_mean.std())
+
+     # append to features the min, max, mean, median, variance, std
+    features.labels.append("min")
+    features.labels.append("max")
+    features.labels.append("mean")
+    features.labels.append("median")
+    features.labels.append("var")
+    features.labels.append("std")
 
     # convert feature list to np array to transpose
     features_np = np.array(features)
@@ -113,7 +164,7 @@ def process_data(path):
         # get_features(df_left_leg, 'a_mag')
 
         
-        df_right_leg_feature = pd.DataFrame(data=features, columns=['min', 'max', 'mean', 'median', 'variance', 'std'])
+        df_right_leg_feature = pd.DataFrame(data=features.values, columns= features.labels)
         print(df_right_leg_feature)
 
         break
